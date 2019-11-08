d444c35dd9614de341e7740a18d4973e0f0b5784
fengguiwei <fengguiwei@itcast.cn>
2019-11-08 17:16:40 +0800

    将文件修改

---
+++ git/src/cn/itcast/FirstClass.java
@@ -0,0 +1,18 @@
+package cn.itcast;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello first git");
+        System.out.println("Hello first git");
+        System.out.println("Hello first git");
+        System.out.println("Hello first git");
+        System.out.println("Hello first git");
+        System.out.println("Hello first git");
+    }
+
+    public  void  test(){
+        int i=0;
+        i+=10;
+
+    }
+}
