737e598bcdb9afe3456cb52f3ce97c758acc967f
fengguiwei <fengguiwei@itcast.cn>
2019-11-08 17:33:06 +0800

    修改FirstClass

--- git/src/cn/itcast/FirstClass.java
+++ git/src/cn/itcast/FirstClass.java
@@ -8,11 +8,12 @@ public class FirstClass {
         System.out.println("Hello first git");
         System.out.println("Hello first git");
         System.out.println("Hello first git");
+        //安安定定多
     }
 
     public  void  test(){
         int i=0;
         i+=10;
-
+        System.out.println(i);
     }
 }
